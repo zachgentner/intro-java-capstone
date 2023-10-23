ca6e663ab148b8caa1041565ed6421d65e0cad09
Zach Gentner <zach.gentner@gmail.com>
2023-10-22 20:48:01 -0400

    added some methods

---
+++ Assessment.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Assessment {
+
+    public Assessment() {
+
+    }
+
+    
+
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.io.*;
+import java.nio.*;
 import java.nio.file.*;
 
 public class Main {
@@ -18,6 +19,43 @@ public class Main {
     return count;
   }
 
+  // Saves the users WPM to a local file for future recovery.
+  public static void saveWPM(int wordsPerMinute) {
+    try {
+        File output = new File("speed.txt");
+        Files.writeString(output.toPath(), String.valueOf(wordsPerMinute), StandardOpenOption.CREATE, 
+                         StandardOpenOption.TRUNCATE_EXISTING);
+      }
+      catch(IOException e) {
+        System.out.println("Error: " + e.getMessage());
+      } 
+  }
+
+  // Loads the user's WPM into the program for continuity.
+  public static int loadWPM() {
+    try {
+        File speed = new File("speed.txt");
+        Scanner fileScanner = new Scanner(speed);
+        if(fileScanner.hasNextLine()) {
+            int result = Integer.parseInt(fileScanner.nextLine());
+            if (result >= 0) { return result; }
+            // return Integer.parseInt(fileScanner.nextLine());
+        }
+    } catch (Exception e) {
+        System.out.println("Previous test scores could not be loaded.");
+    }
+        return -1;
+  }
+
+  // Manually sets the WPM in order to bypass the assessment.
+  public static void manualWPM(int wordsPerMinute) {
+    saveWPM(wordsPerMinute);
+  }
+
+  public static int minutesToRead(int words, int speed) {
+    return words / speed;
+  }
+
   public static void timeToRead(int words, int speed) {
     int minutes = words / speed;
 
@@ -29,34 +67,53 @@ public class Main {
       System.out.println("Time to read: " + minutes + " minutes.");
     }
   }
+
+  public static void timeByWords(int words) {
+    System.out.println("150WPM (Slow): " + minutesToRead(words, 150) + " minutes");
+    System.out.println("250WPM (Avg):  " + minutesToRead(words, 250) + " minutes");
+    System.out.println("400WPM (Fast): " + minutesToRead(words, 400) + " minutes");
+    System.out.println("600WPM (Pro):  " + minutesToRead(words, 600) + " minutes");
+  }
+
+  public static void timeBySpeed(int speed) {
+    System.out.println("500 Words:   " + minutesToRead(500, speed) + " minutes");
+    System.out.println("1000 Words:  " + minutesToRead(1000, speed) + " minutes");
+    System.out.println("2500 Words:  " + minutesToRead(2500, speed) + " minutes");
+    System.out.println("5000 Words:  " + minutesToRead(5000, speed) + " minutes");
+    System.out.println("7500 Words:  " + minutesToRead(7500, speed) + " minutes");
+    System.out.println("10000 Words: " + minutesToRead(10000, speed) + " minutes");
+  }
+
+  public static void startProgram(Scanner input) {
+      System.out.println("Welcome. Please enter a reading speed.");
+
+  }
   
   public static void main (String[] args) {
 
     Scanner input = new Scanner(System.in);
     int speed = 0;
-    File file = new File("./samples/babbage.txt");
+    File file = new File("samples/10000words.txt");
     
-    System.out.println("Welcome. Please enter a reading speed.");
 
-    while(!(speed > 0) || !(speed <= 600)) {
-      try {
-        int temp = input.nextInt();
-        if (temp > 0 && temp < 600) {
-          speed = temp;
-        } else {
-          System.out.println("Please enter a valid number.");
-        }
-      } catch(Exception e) {
-        System.out.println("Please enter a valid number.");
-        input.nextLine();
-      }
-    }
+    // while(!(speed > 0) || !(speed <= 600)) {
+    //   try {
+    //     int temp = input.nextInt();
+    //     if (temp > 0 && temp < 600) {
+    //       speed = temp;
+    //     } else {
+    //       System.out.println("Please enter a valid number.");
+    //     }
+    //   } catch(Exception e) {
+    //     System.out.println("Please enter a valid number.");
+    //     input.nextLine();
+    //   }
+    // }
 
-    System.out.println("Number of words: " + numberOfWords(file));
-    System.out.println("Reading speed: " + speed);
-    System.out.println("Equation: " + (numberOfWords(file) / speed));
-    timeToRead(numberOfWords(file), speed);
-    
-    
+    saveWPM(400);
+    manualWPM(100);
+    System.out.println(loadWPM());
+
+
   }
 }
